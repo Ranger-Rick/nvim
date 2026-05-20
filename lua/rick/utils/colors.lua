e9ce513f98e98d7a0c340483e43a3688f128782f
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-05-20 16:04:07 -0500

    Included a user command to enable / disable blur

--- lua/rick/utils/colors.lua
+++ lua/rick/utils/colors.lua
@@ -12,14 +12,18 @@ function EnableBlur()
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-function DisableBlur(color)
-    Colors(color);
-end
-
 function LineNumberColors()
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
     vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
 end
+
+vim.api.nvim_create_user_command('Blur', function ()
+    EnableBlur()
+end, { desc = 'Enable a background blur' })
+
+vim.api.nvim_create_user_command('NoBlur', function ()
+    Colors()
+end, { desc = 'Disable the background blur' })
 
 Colors()
